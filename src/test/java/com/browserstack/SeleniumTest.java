a3df526a7ce82b360c12c28dc932fbb3355fd8f0
Kamalpreet Kaur <kmlkaur73@gmail.com>
2023-04-04 22:39:48 +0530

    add: it's a test again and again

--- src/test/java/com/browserstack/SeleniumTest.java
+++ src/test/java/com/browserstack/SeleniumTest.java
@@ -12,7 +12,7 @@ public class SeleniumTest {
     @BeforeMethod(alwaysRun = true)
     @SuppressWarnings("unchecked")
     public void setUp() throws Exception {
-        System.out.println("IT'S A TEST!!");
+        System.out.println("IT'S A TEST AGAIN!!");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
